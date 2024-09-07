c81f15f4047cbf33a6cc17c22c1481a24fc5b7b3
Azo <azadp723@gmail.com>
2024-09-07 19:00:25 +0300

    dfdf

--- src/components/Icerik.jsx
+++ src/components/Icerik.jsx
@@ -3,29 +3,58 @@ import styled from "styled-components";
 
 
 const Icerikler=styled.div`
-width: 100vw;
-height: 50vh;
-background-color: #19cf93;
+width: 86vw;
+height: 60vh;
 display: flex;
-flex-direction: row;
+margin-top: 1vh;
+margin-left: 8vw;
 
 .haber-1{
     width: 50vw;
-    height: 50vh;
-    background-color: #ffffff;
+    height: 60vh;
+    background-color: #f50000;
+}
+.haber-container{
+    display: flex;
+    flex-direction: column;
 }
 .haber-2{
-    width: 20vw;
-    height: 50vh;
-    background-color: #b10909;
+    width: 36vw;
+    height: 15vh;
+    background-color: #007bff;
+    margin-bottom: 0.1vh;
 }
+.haber-2>.logo{
+    width: 6vw;
+    height: 12vh;
+    background-color: #383838;
+    border-radius:50%;
+    margin-left: -3vw;
+    margin-top: 2vh;
+}
+.haber-3{
+    width: 36vw;
+    height: 30vh;
+    background-color: #eeff00;
+    margin-bottom: 0.1vh;
+}
+
 `
 
 export default function Icerik(){
     return(
         <Icerikler>
             <div className="haber-1"></div>
-            <div className="haber-2"></div>
+            <div className="haber-container">
+            <div className="haber-2">
+                <div className="logo"></div>
+            </div>
+            <div className="haber-2">
+                <div className="logo"></div>
+            </div>
+            <div className="haber-3"></div>
+            </div>
+          
         </Icerikler>
     )
 }
